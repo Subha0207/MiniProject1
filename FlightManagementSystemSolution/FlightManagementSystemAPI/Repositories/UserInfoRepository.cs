a36e23d001fb424ea29f6a49135db05d99468eef
Subha0207 <subhashinisv@presidio.com>
2024-05-26 23:39:25 +0530

    Added Context files and DB Connection

---
+++ FlightManagementSystemSolution/FlightManagementSystemAPI/Repositories/UserInfoRepository.cs
@@ -0,0 +1,147 @@
+﻿
+using FlightManagementSystemAPI.Contexts;
+using FlightManagementSystemAPI.Exceptions;
+using FlightManagementSystemAPI.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
+
+namespace FlightManagementSystemAPI.Repositories
+{
+    public class UserInfoRepository : IRepository<int, UserInfo>
+    {
+        private readonly FlightManagementContext _context;
+
+        public  UserInfoRepository(FlightManagementContext context)
+        {
+            _context = context;
+        }
+        public async Task<UserInfo> Add(UserInfo item)
+        {
+            try
+            {
+                var existingUserByEmail = await _context.UserInfos.FirstOrDefaultAsync(u => u.Email == item.Email);
+                if (existingUserByEmail != null)
+                {
+                    throw new UserAlreadyExistsException("A user with the same email already exists.");
+                }
+
+                
+
+                string emailRegex = @"^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$";
+
+                if (!Regex.IsMatch(item.Email, emailRegex))
+                {
+                    throw new ArgumentException("Invalid email format.");
+                }
+
+                _context.Add(item);
+                await _context.SaveChangesAsync();
+                return item;
+            }
+            catch (UserAlreadyExistsException ex)
+            {
+                throw new UserInfoException("Error: " + ex.Message, ex);
+            }
+            
+            catch (ArgumentException ex)
+            {
+                throw new UserInfoException("Error: " + ex.Message, ex);
+            }
+            catch (Exception ex)
+            {
+                throw new UserInfoException("Error occurred while adding user info.", ex);
+            }
+        }
+
+
+        public async  Task<UserInfo> Delete(int key)
+        {
+            try
+            {
+                var userInfo = await Get(key);
+                _context.Remove(userInfo);
+                await _context.SaveChangesAsync(true);
+                return userInfo;
+            }
+            catch(UserNotFoundException ex)
+            {
+                throw new UserInfoException("Error occurred while deleting user info: " + ex.Message, ex);
+
+            }
+            catch(Exception ex)
+            {
+                throw new UserInfoException("Error while deleting user info",ex);
+            }
+        }
+
+        public async Task<UserInfo> Get(int key)
+        {
+            try
+            {
+                var userInfo = await _context.UserInfos.FirstOrDefaultAsync(u => u.UserId == key);
+                if (userInfo != null)
+                {
+                    return userInfo;
+                }
+                throw new UserNotFoundException("No user found with given user Id");
+            }
+            catch(UserNotFoundException ex)
+            {
+                throw new UserInfoException("Error occurred while getting user info: " + ex.Message, ex);
+            }
+            catch (Exception ex)
+            {
+                throw new UserInfoException("Error occurred while getting user info.", ex);
+            }
+        }
+
+        public async Task<IEnumerable<UserInfo>> GetAll()
+        {
+            try
+            {
+                var userInfos = await _context.UserInfos.ToListAsync();
+                if (userInfos.Count <= 0)
+                {
+
+                    throw new UserNotFoundException("No Users found");
+                }
+                return userInfos;
+            }
+            catch(UserNotFoundException ex)
+            {
+
+
+                throw new UserInfoException("Error occurred while getting user infos: " + ex.Message, ex);
+            }
+            catch (Exception ex)
+            {
+                throw new UserInfoException("Error occurred while getting user infos.", ex);
+            }
+
+        }
+
+        
+
+        public async Task<UserInfo> Update(UserInfo item)
+        {
+            try
+            {
+                var userInfo = await Get(item.UserId);
+                _context.Update(userInfo);
+                await _context.SaveChangesAsync(true);
+                return userInfo;
+
+            }
+            catch(UserNotFoundException ex)
+            {
+                throw new UserInfoException("Error occurred while updating user info. User info not found: " + ex.Message, ex);
+
+            }
+            catch (Exception ex)
+            {
+                throw new UserInfoException("Error occurred while updating user detail. User info not found: " , ex);
+
+            }
+        }
+    }
+}
---
+++ FlightManagementSystemSolution/FlightManagementSystemAPI/Repositories/UserRepository.cs
@@ -0,0 +1,143 @@
+﻿using FlightManagementSystemAPI.Contexts;
+using FlightManagementSystemAPI.Exceptions;
+using FlightManagementSystemAPI.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
+
+namespace FlightManagementSystemAPI.Repositories
+{
+    public class UserRepository : IRepository<int, User>
+    {
+        private readonly FlightManagementContext _context;
+
+        public UserRepository(FlightManagementContext context)
+        {
+            _context = context;
+        }
+        public async Task<User> Add(User item)
+        {
+            try
+            {
+                var existingUserByEmail = await _context.Users.FirstOrDefaultAsync(u => u.Email == item.Email);
+                if (existingUserByEmail != null)
+                {
+                    throw new UserAlreadyExistsException("A user with the same email already exists.");
+                }
+
+                if (item.Password != item.ConfirmPassword)
+                {
+
+                    throw new PasswordException("Password and confirm password must be the same");
+                }
+                
+                string emailRegex = @"^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$";
+
+                if (!Regex.IsMatch(item.Email, emailRegex))
+                {
+                    throw new EmailFormatException("Invalid email format.");
+                }
+                _context.Add(item);
+                await _context.SaveChangesAsync();
+                return item;
+            }
+            catch (UserAlreadyExistsException ex)
+            {
+                throw new UserException( ex.Message, ex);
+            }
+            catch(EmailFormatException ex)
+            {
+                throw new UserException( ex.Message, ex);
+            }
+            catch (PasswordException ex)
+            {
+
+                throw new UserException(ex.Message, ex);
+            }
+            catch (Exception ex)
+            {
+                throw new UserException("Error occurred while adding user info.", ex);
+            }
+        }
+
+        public async Task<User> Delete(int key)
+        {
+            try
+            {
+
+                var user = await Get(key);
+                _context.Remove(user);
+                await _context.SaveChangesAsync(true);
+                return user;
+            }
+            catch (UserNotFoundException ex)
+            {
+                throw new UserException("Error occurred while deleting user " + ex.Message, ex);
+            }
+            catch (Exception ex)
+            {
+                throw new UserException("Error occurred while deleting user.", ex);
+            }
+        }
+
+        public async Task<User> Get(int key)
+        {
+            try
+            {
+                var user = await _context.Users.FirstOrDefaultAsync(u => u.UserId == key);
+                if (user != null)
+                    return user;
+                throw new UserNotFoundException("No such user found");
+             
+            }
+            catch (UserNotFoundException ex)
+            {
+                throw new UserException("Error occurred while getting user" + ex.Message, ex);
+            }
+            catch (Exception ex)
+            {
+                throw new UserException("Error occurred while getting user.", ex);
+            }
+        }
+
+        public async Task<IEnumerable<User>> GetAll()
+        {
+            try
+            {
+                var users = await _context.Users.ToListAsync();
+                if (users.Count <= 0)
+                {
+                    throw new UserNotFoundException("There is not user Present");
+                }
+                return users;
+            }
+            catch (UserNotFoundException ex)
+            {
+                throw new UserException("Error occured while retrieving users" + ex.Message, ex);
+            }
+            catch (Exception ex)
+            {
+                throw new UserException("Error occurred while retrieving users.", ex);
+            }
+        }
+
+        
+        public async Task<User> Update(User item)
+        {
+            try
+            {
+                var user = await Get(item.UserId);
+                _context.Update(user);
+                await _context.SaveChangesAsync(true);
+                return user;
+            }
+            catch (UserNotFoundException ex)
+            {
+                throw new UserException("Error occurred while updating user. User not found." + ex.Message, ex);
+            }
+            catch (Exception ex)
+            {
+                throw new UserException("Error occurred while updating user.", ex);
+            }
+        }
+    }
+}
